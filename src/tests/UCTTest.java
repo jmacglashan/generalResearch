3374ce09c1f4780b06d81546a28d6c899b6b9e4a
jmacglashan <jmacglashan@gmail.com>
2015-06-30 18:09:47 -0400

    updated with UCT fix

--- src/tests/UCTTest.java
+++ src/tests/UCTTest.java
@@ -2,9 +2,11 @@ package tests;
 
 import burlap.behavior.singleagent.EpisodeAnalysis;
 import burlap.behavior.singleagent.EpisodeSequenceVisualizer;
+import burlap.behavior.singleagent.Policy;
 import burlap.behavior.singleagent.planning.commonpolicies.GreedyQPolicy;
 import burlap.behavior.singleagent.planning.stochastic.montecarlo.uct.UCT;
 import burlap.behavior.statehashing.DiscreteStateHashFactory;
+import burlap.debugtools.RandomFactory;
 import burlap.domain.singleagent.gridworld.*;
 import burlap.oomdp.auxiliary.StateParser;
 import burlap.oomdp.core.Domain;
@@ -12,12 +14,21 @@ import burlap.oomdp.core.State;
 import burlap.oomdp.core.TerminalFunction;
 import burlap.oomdp.visualizer.Visualizer;
 
+import java.util.List;
+
 /**
  * @author James MacGlashan.
  */
 public class UCTTest {
 
 	public static void main(String [] args){
+
+		//errorTest();
+		exampleRun();
+
+	}
+
+	public static void exampleRun(){
 
 		//create domain
 		GridWorldDomain gwd = new GridWorldDomain(5, 5);
@@ -46,6 +57,33 @@ public class UCTTest {
 		Visualizer v = GridWorldVisualizer.getVisualizer(gwd.getMap());
 		EpisodeSequenceVisualizer evis = new EpisodeSequenceVisualizer(v, domain, sp, "uct");
 
+	}
+
+	public static void errorTest(){
+
+		//create domain
+		GridWorldDomain gwd = new GridWorldDomain(5, 5);
+		gwd.makeEmptyMap();
+		Domain domain = gwd.generateDomain();
+
+		//set up task to solve in the domain
+		GridWorldRewardFunction rf = new GridWorldRewardFunction(domain, 0);
+		rf.setReward(4, 4, 1.);
+		TerminalFunction tf = new GridWorldTerminalFunction(4, 4);
+		State s = GridWorldDomain.getOneAgentNoLocationState(domain, 0, 0);
+
+		//set up planner/policy
+		//uct using a planning horizon of 12, 1000 rollouts each time it plans, and an exploration bias constant of 2
+		UCT uct = new UCT(domain, rf, tf, 0.99, new DiscreteStateHashFactory(), 1, 1, 2);
+		GreedyQPolicy p = new GreedyQPolicy(uct);
+
+		RandomFactory.seedMapped(589449, 12);
+
+		List<Policy.ActionProb> aps = p.getActionDistributionForState(s);
+
+		for(Policy.ActionProb ap : aps){
+			System.out.println(ap.pSelection);
+		}
 
 	}
 
